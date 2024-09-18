c7a0b782f354027c039492c7021e68858fdab543
nnamdi nwaocha <nnamdinwaocha@gmail.com>
2024-09-18 11:17:22 +0100

    feat():  get random quote, get all quotes, get quote by person, add new quote

--- server.js
+++ server.js
@@ -1,10 +1,50 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const { quotes } = require('./data');
-const { getRandomElement } = require('./utils');
+const { quotes } = require("./data");
+const { getRandomElement, findQuotesByPerson } = require("./utils");
 
 const PORT = process.env.PORT || 4001;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
+const quotesRouter = express.Router();
+app.use("/api/quotes", quotesRouter);
 
+quotesRouter.get("/random", (req, res, next) => {
+  const randomQuote = getRandomElement(quotes);
+  console.log(randomQuote);
+  res.send({
+    quote: randomQuote,
+  });
+});
+
+quotesRouter.get("/", (req, res, next) => {
+  const person = req.query.person;
+  if (person) {
+    const quotesByPerson = findQuotesByPerson(quotes, person);
+    res.send({
+      quotes: quotesByPerson,
+    });
+  } else {
+    res.send({
+      quotes,
+    });
+  }
+});
+
+quotesRouter.post("/", (req, res, next) => {
+  const { quote, person } = req.query;
+  if (quote && person) {
+    const newQuote = { quote, person };
+    quotes.push(newQuote);
+    res.send({
+      quote: newQuote,
+    });
+  } else {
+    res.status(404).send();
+  }
+});
+
+app.listen(PORT, () => {
+  console.log("we live people");
+});
--- utils.js
+++ utils.js
@@ -1,8 +1,16 @@
-const getRandomElement = arr => {
-  if (!Array.isArray(arr)) throw new Error('Expected an array');
+const getRandomElement = (arr) => {
+  if (!Array.isArray(arr)) throw new Error("Expected an array");
   return arr[Math.floor(Math.random() * arr.length)];
-}
+};
+
+const findQuotesByPerson = (arr, person) => {
+  const quotes = arr.filter((quote) => {
+    return quote.person === person;
+  });
+  return quotes;
+};
 
 module.exports = {
-  getRandomElement
+  getRandomElement,
+  findQuotesByPerson,
 };
